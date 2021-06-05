ba4cb7bbc1116d46f7c194380574bbe913ff3c9f
Chelsey Morris <chelseymorris91@yahoo.com>
2021-06-05 16:03:31 -0700

    styling project boxes

--- portfolio/src/components/Projects/index.js
+++ portfolio/src/components/Projects/index.js
@@ -7,30 +7,41 @@ import "./style.css"
 function Projects () {
     return ( 
         <div className="projectBox"> 
-        <h1 className="row">Recent Projects</h1>
+
+        <h1 className="row" style={{marginBottom: "10rem"}}>Recent Projects</h1>
+
+        <div className="row">
           <div className="project-card" style={{width: "30rem"}}>
-            <a href="https://radiant-harbor-61772.herokuapp.com/">The Move</a>
+            <a className="appLink" href="https://radiant-harbor-61772.herokuapp.com/">The Move</a>
                 <a className="repo-img"href="https://github.com/cmorris91/the-move" style={{fontSize: "15px", textDecoration: "none"}}>
                 <img src={Image} className="card-img-top" alt="The-Move-App"/> 
                 <div className="overlay">Click to see the repo</div>
             </a>
           </div>
+          <h2 className="col description">simple application</h2>
+        </div>
 
-          <div className="project-card row"  id="card2">
-            <a href="https://vast-dawn-89998.herokuapp.com/">Event Site</a>
-                <a class="repo-img" href="https://github.com/AFF321/JumpIn" style={{fontSize: "15px", textDecoration: "none"}}>
+        <div id="card2" className="row">
+          <h2 className="col description">simple application</h2>
+          <div className="project-card" style={{width: "30rem"}}>
+                <a className="repo-img" href="https://github.com/AFF321/JumpIn" style={{fontSize: "15px", textDecoration: "none"}}>
+                <a className="appLink" href="https://vast-dawn-89998.herokuapp.com/">JumpIn</a>
                 <img src={Image2} class="card-img-top" alt="JumpIn"/> 
                 <div class="overlay">Click to see the repo</div>  
             </a>
           </div>
+        </div>
 
+        <div className="row">
           <div className="project-card" style={{width: "30rem"}}>
-            <a href="https://cmorris91.github.io/group-project-1/">Travel Page</a>
-                <a class="repo-img" href="https://github.com/cmorris91/group-project-1" style={{fontSize: "15px", textDecoration: "none"}}>
+            <a className="appLink" href="https://cmorris91.github.io/group-project-1/">We Want Travel</a>
+                <a className="repo-img" href="https://github.com/cmorris91/group-project-1" style={{fontSize: "15px", textDecoration: "none"}}>
                 <img src={Image3} className="card-img-top" alt="Travel Page"/>
                     <div class="overlay">Click to see the repo</div>
             </a>
           </div>
+          <h2 className="col description">simple application</h2>
+        </div>
 
         </div>
     )
