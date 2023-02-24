d14feda4eb8b5c18c0df46d81d949cb68392f93f
Chelsey Morris <chelseymorris91@yahoo.com>
2023-02-23 19:38:30 -0800

    adding switch routes for projects

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -6,22 +6,32 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import AboutMe from "./components/About-me";
 import Contact from "./components/Contact";
+import JumpIn from "./pages/jumpin";
+import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
 import "./style.css";
 
 
 function App() {
 
     return (
+    
+        <Router>
+            <div> 
+                <Title/>
+                <Header/>
+                <Nav/>
+                <AboutMe/>
+                <Projects/>
+                <Skills/>
+                <Contact/>
+            </div>
+            <div>
+                <Routes>
+                    <Route path="/jumpin" element={JumpIn}/>
+                </Routes>
+            </div>
+        </Router>
 
-        <div> 
-            <Title/>
-            <Header/>
-            <Nav/>
-            <AboutMe/>
-            <Projects/>
-            <Skills/>
-            <Contact/>
-        </div>
     
     )
 }
---
+++ portfolio/src/pages/jumpin.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import Image2 from "./Images/JumpIn.jpg";
+
+function JumpIn () {
+    return (
+        <div>
+            <p>hello</p>
+            {/* <image src={Image2}></image> */}
+        </div>
+    )
+}
+
+export default JumpIn;
